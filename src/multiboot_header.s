2f1b2bf96bab6bde864b2da1eb1e00384e02c986
zakikhan25 <zkhan25@luc.edu>
2025-12-04 10:59:42 -0600

    HW1:  multiboot_header.s

--- src/multiboot_header.s
+++ src/multiboot_header.s
@@ -10,3 +10,28 @@
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
+
+# Entry point
+.section .text
+.global _start
+.type _start, @function
+
+_start:
+    # Set up stack
+    mov $stack_top, %esp
+    
+    # Call kernel_main
+    call kernel_main
+    
+    # Hang if kernel_main returns
+halt:
+    cli
+    hlt
+    jmp halt
+
+# Stack space
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384  # 16 KB stack
+stack_top:
