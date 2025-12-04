6e9ba3b933f00aae5e53c02c982ae45d89b1b28f
zakikhan25 <zkhan25@luc.edu>
2025-12-04 10:58:47 -0600

    HW1:  rprintf.c

--- src/rprintf.c
+++ src/rprintf.c
@@ -1,228 +1,283 @@
+/*---------------------------------------------------*/
+/* Public Domain version of printf                   */
+/*                                                   */
+/* Rud Merriam, Compsult, Inc. Houston, Tx.          */
+/*                                                   */
+/* For Embedded Systems Programming, 1991            */
+/*                                                   */
+/*---------------------------------------------------*/
+
 #include "rprintf.h"
+/*---------------------------------------------------*/
+/* The purpose of this routine is to output data the */
+/* same as the standard printf function without the  */
+/* overhead most run-time libraries involve. Usually */
+/* the printf brings in many kiolbytes of code and   */
+/* that is unacceptable in most embedded systems.    */
+/*---------------------------------------------------*/
+
+static func_ptr out_char;
+static int do_padding;
+static int left_flag;
+static int len;
+static int num1;
+static int num2;
+static char pad_character;
+
+size_t strlen(const char *str) {
+    unsigned int len = 0;
+    while(str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+int tolower(int c) {
+    if(c < 'a') { // Check if c is uppercase
+        c -= 'a' - 'A';
+    }
+    return c;
+}
 
 int isdig(int c) {
-    return (c >= '0' && c <= '9');
-}
-
-static int skip_atoi(const char **s) {
-    int i = 0;
-    while (isdig(**s))
-        i = i*10 + *((*s)++) - '0';
-    return i;
-}
-
-#define ZEROPAD 1
-#define SIGN    2
-#define PLUS    4
-#define SPACE   8
-#define LEFT    16
-#define SPECIAL 32
-#define SMALL   64
-
-#define do_div(n,base) ({ \
-int __res; \
-__res = ((unsigned long) n) % (unsigned) base; \
-n = ((unsigned long) n) / (unsigned) base; \
-__res; })
-
-static char * number(char * str, int num, int base, int size, int precision, int type) {
-    char c,sign,tmp[36];
-    const char *digits="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int i;
-
-    if (type&SMALL) digits="0123456789abcdefghijklmnopqrstuvwxyz";
-    if (type&LEFT) type &= ~ZEROPAD;
-    if (base<2 || base>36)
+    if((c >= '0') && (c <= '9')){
+        return 1;
+    } else {
         return 0;
-    c = (type & ZEROPAD) ? '0' : ' ' ;
-    if (type&SIGN && num<0) {
-        sign='-';
-        num = -num;
-    } else
-        sign=(type&PLUS) ? '+' : ((type&SPACE) ? ' ' : 0);
-    if (sign) size--;
-    if (type&SPECIAL)
-        if (base==16) size -= 2;
-        else if (base==8) size--;
-    i=0;
-    if (num==0)
-        tmp[i++]='0';
-    else while (num!=0)
-        tmp[i++]=digits[do_div(num,base)];
-    if (i>precision) precision=i;
-    size -= precision;
-    if (!(type&(ZEROPAD+LEFT)))
-        while(size-->0)
-            *str++ = ' ';
-    if (sign)
-        *str++ = sign;
-    if (type&SPECIAL)
-        if (base==8)
-            *str++ = '0';
-        else if (base==16) {
-            *str++ = '0';
-            *str++ = digits[33];
-        }
-    if (!(type&LEFT))
-        while(size-->0)
-            *str++ = c;
-    while(i<precision--)
-        *str++ = '0';
-    while(i-->0)
-        *str++ = tmp[i];
-    while(size-->0)
-        *str++ = ' ';
-    return str;
-}
-
-void esp_vprintf( const func_ptr f_ptr, charptr ctrl, va_list argp) {
-    int len;
-    int i;
-    char *s;
-    int *ip;
-    int flags;
-    int field_width;
-    int precision;
-    int qualifier;
-    char buf[512];
-    char *str;
-
-    for (str = buf; *ctrl; ++ctrl) {
-        if (*ctrl != '%') {
-            *str++ = *ctrl;
-            continue;
-        }
-
-        flags = 0;
-        repeat:
-            ++ctrl;
-            switch (*ctrl) {
-                case '-': flags |= LEFT; goto repeat;
-                case '+': flags |= PLUS; goto repeat;
-                case ' ': flags |= SPACE; goto repeat;
-                case '#': flags |= SPECIAL; goto repeat;
-                case '0': flags |= ZEROPAD; goto repeat;
-            }
-
-        field_width = -1;
-        if (isdig(*ctrl))
-            field_width = skip_atoi(&ctrl);
-        else if (*ctrl == '*') {
-            field_width = va_arg(argp, int);
-            if (field_width < 0) {
-                field_width = -field_width;
-                flags |= LEFT;
-            }
-        }
-
-        precision = -1;
-        if (*ctrl == '.') {
-            ++ctrl;
-            if (isdig(*ctrl))
-                precision = skip_atoi(&ctrl);
-            else if (*ctrl == '*') {
-                precision = va_arg(argp, int);
-            }
-            if (precision < 0)
-                precision = 0;
-        }
-
-        qualifier = -1;
-        if (*ctrl == 'h' || *ctrl == 'l' || *ctrl == 'L') {
-            qualifier = *ctrl;
-            ++ctrl;
-        }
-
-        switch (*ctrl) {
-            case 'c':
-                if (!(flags & LEFT))
-                    while (--field_width > 0)
-                        *str++ = ' ';
-                *str++ = (unsigned char) va_arg(argp, int);
-                while (--field_width > 0)
-                    *str++ = ' ';
-                break;
-
-            case 's':
-                s = va_arg(argp, char *);
-                if (!s)
-                    s = "<NULL>";
-                len = 0;
-                while (s[len]) len++;
-                if (precision >= 0 && len > precision)
-                    len = precision;
-
-                if (!(flags & LEFT))
-                    while (len < field_width--)
-                        *str++ = ' ';
-                for (i = 0; i < len; ++i)
-                    *str++ = *s++;
-                while (len < field_width--)
-                    *str++ = ' ';
-                break;
-
-            case 'o':
-                str = number(str, va_arg(argp, unsigned long), 8,
-                    field_width, precision, flags);
-                break;
-
-            case 'p':
-                if (field_width == -1) {
-                    field_width = 8;
-                    flags |= ZEROPAD;
-                }
-                str = number(str,
-                    (unsigned long) va_arg(argp, void *), 16,
-                    field_width, precision, flags);
-                break;
-
-            case 'x':
-                flags |= SMALL;
-            case 'X':
-                str = number(str, va_arg(argp, unsigned long), 16,
-                    field_width, precision, flags);
-                break;
-
-            case 'd':
-            case 'i':
-                flags |= SIGN;
-            case 'u':
-                str = number(str, va_arg(argp, unsigned long), 10,
-                    field_width, precision, flags);
-                break;
-
-            case 'n':
-                ip = va_arg(argp, int *);
-                *ip = (str - buf);
-                break;
-
-            default:
-                if (*ctrl != '%')
-                    *str++ = '%';
-                if (*ctrl)
-                    *str++ = *ctrl;
-                else
-                    --ctrl;
-                break;
-        }
     }
-    *str = '\0';
-
-    for (i = 0; buf[i]; i++)
-        f_ptr(buf[i]);
 }
 
-void esp_printf( const func_ptr f_ptr, charptr ctrl, ...) {
-    va_list argp;
-    va_start(argp, ctrl);
-    esp_vprintf(f_ptr, ctrl, argp);
-    va_end(argp);
+
+
+
+/*---------------------------------------------------*/
+/*                                                   */
+/* This routine puts pad characters into the output  */
+/* buffer.                                           */
+/*                                                   */
+static void padding( const int l_flag)
+{
+   int i;
+
+   if (do_padding && l_flag && (len < num1))
+      for (i=len; i<num1; i++)
+          out_char( pad_character);
+   }
+
+/*---------------------------------------------------*/
+/*                                                   */
+/* This routine moves a string to the output buffer  */
+/* as directed by the padding and positioning flags. */
+/*                                                   */
+static void outs( charptr lp)
+{
+   if(lp == NULL)
+      lp = "(null)";
+   /* pad on left if needed                          */
+   len = strlen( lp);
+   padding( !left_flag);
+
+   /* Move string to the buffer                      */
+   while (*lp && num2--)
+      out_char( *lp++);
+
+   /* Pad on right if needed                         */
+   len = strlen( lp);
+   padding( left_flag);
+   }
+
+/*---------------------------------------------------*/
+/*                                                   */
+/* This routine moves a number to the output buffer  */
+/* as directed by the padding and positioning flags. */
+/*                                                   */
+static void outnum(unsigned int num, const int base)
+{
+   charptr cp;
+   int negative;
+   char outbuf[32];
+   const char digits[] = "0123456789ABCDEF";
+
+   /* Check if number is negative                    */
+   /* NAK 2009-07-29 Negate the number only if it is not a hex value. */
+   if (num < 0L && base != 16L) {
+      negative = 1;
+      num = -num;
+      }
+   else
+      negative = 0;
+
+   /* Build number (backwards) in outbuf             */
+   cp = outbuf;
+   do {
+      *cp++ = digits[num % base];
+      } while ((num /= base) > 0);
+   if (negative)
+      *cp++ = '-';
+   *cp-- = 0;
+
+   /* Move the converted number to the buffer and    */
+   /* add in the padding where needed.               */
+   len = strlen(outbuf);
+   padding( !left_flag);
+   while (cp >= outbuf)
+      out_char( *cp--);
+   padding( left_flag);
 }
 
-void esp_sprintf(char *buf, char *ctrl, ...) {
-    // Not implemented for kernel use
+/*---------------------------------------------------*/
+/*                                                   */
+/* This routine gets a number from the format        */
+/* string.                                           */
+/*                                                   */
+static int getnum( charptr* linep)
+{
+   int n;
+   charptr cp;
+
+   n = 0;
+   cp = *linep;
+   while (isdig((int)*cp))
+      n = n*10 + ((*cp++) - '0');
+   *linep = cp;
+   return(n);
 }
 
-void printk(charptr ctrl, ...) {
-    // Not implemented for kernel use
+/*---------------------------------------------------*/
+/*                                                   */
+/* This routine operates just like a printf/sprintf  */
+/* routine. It outputs a set of data under the       */
+/* control of a formatting string. Not all of the    */
+/* standard C format control are supported. The ones */
+/* provided are primarily those needed for embedded  */
+/* systems work. Primarily the floaing point         */
+/* routines are omitted. Other formats could be      */
+/* added easily by following the examples shown for  */
+/* the supported formats.                            */
+/*                                                   */
+
+void esp_printf( const func_ptr f_ptr, charptr ctrl, ...)
+{
+  va_list args;
+  va_start(args, *ctrl);
+  esp_vprintf(f_ptr, ctrl, args);
+  va_end( args );
+  
 }
+
+void esp_vprintf( const func_ptr f_ptr, charptr ctrl, va_list argp)
+{
+
+   int long_flag;
+   int dot_flag;
+
+   char ch;
+   //va_list argp;
+
+   //va_start( argp, ctrl);
+   out_char = f_ptr;
+
+   for ( ; *ctrl; ctrl++) {
+
+      /* move format string chars to buffer until a  */
+      /* format control is found.                    */
+      if (*ctrl != '%') {
+         out_char(*ctrl);
+         continue;
+         }
+
+      /* initialize all the flags for this format.   */
+      dot_flag   =
+      long_flag  =
+      left_flag  =
+      do_padding = 0;
+      pad_character = ' ';
+      num2=32767;
+
+try_next:
+      ch = *(++ctrl);
+
+      if (isdig((int)ch)) {
+         if (dot_flag)
+            num2 = getnum(&ctrl);
+         else {
+            if (ch == '0')
+               pad_character = '0';
+
+            num1 = getnum(&ctrl);
+            do_padding = 1;
+         }
+         ctrl--;
+         goto try_next;
+      }
+
+      switch (tolower((int)ch)) {
+         case '%':
+              out_char( '%');
+              continue;
+
+         case '-':
+              left_flag = 1;
+              break;
+
+         case '.':
+              dot_flag = 1;
+              break;
+
+         case 'l':
+              long_flag = 1;
+              break;
+	
+         case 'i':
+         case 'd':
+              if (long_flag || ch == 'D') {
+                 outnum( va_arg(argp, long), 10L);
+                 continue;
+                 }
+              else {
+                 outnum( va_arg(argp, int), 10L);
+                 continue;
+                 }
+         case 'x':
+              outnum( (long)va_arg(argp, int), 16L);
+              continue;
+
+         case 's':
+              outs( va_arg( argp, charptr));
+              continue;
+
+         case 'c':
+              out_char( va_arg( argp, int));
+              continue;
+
+         case '\\':
+              switch (*ctrl) {
+                 case 'a':
+                      out_char( 0x07);
+                      break;
+                 case 'h':
+                      out_char( 0x08);
+                      break;
+                 case 'r':
+                      out_char( 0x0D);
+                      break;
+                 case 'n':
+                      out_char( 0x0D);
+                      out_char( 0x0A);
+                      break;
+                 default:
+                      out_char( *ctrl);
+                      break;
+                 }
+              ctrl++;
+              break;
+
+         default:
+              continue;
+         }
+      goto try_next;
+      }
+   va_end( argp);
+   }
+
+/*---------------------------------------------------*/
